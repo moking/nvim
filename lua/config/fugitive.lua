e58cd02d1f1658182975174449c9ee2128cc1d48
Jason Zeng <zrzeng@gmail.com>
2025-03-04 18:14:42 +0800

    minor cleanup

--- lua/config/fugitive.lua
+++ lua/config/fugitive.lua
@@ -2,8 +2,8 @@
 vim.cmd([[let g:nremap = {"gO": "o"}]])
 
 function GitHistoryForVisualLines()
-	local start_row, start_col = unpack(vim.api.nvim_buf_get_mark(0, '<'))
-	local end_row, end_col = unpack(vim.api.nvim_buf_get_mark(0, '>'))
+	local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, '<'))
+	local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, '>'))
 	vim.api.nvim_command(string.format("vertical Git log --oneline --no-patch -L%d,%d:%%", start_row, end_row))
 	vim.cmd("wincmd o")
         vim.api.nvim_buf_set_option(0, 'bufhidden', "hide")
