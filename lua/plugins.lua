99c93af5e6c1d046e80a9672cc819ad5bf331cbe
Jason Zeng <zrzeng@gmail.com>
2025-03-06 10:29:41 +0800

    plugin.lua: cleanup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -151,18 +151,6 @@ return {
 		config = function() require("config.mini-indentscope") end,
 	},
 
---	{
---		"x48Jason/glance",
---		opts = {
---			gitee = {
---				token_file = "~/.token.gitee",
---				repo = "openeuler/kernel",
---			},
---			patchdiff = "diffonly",
---			q_quit_log = "off",
---		},
---	},
-
 	{
 		"x56Jason/glance.nvim",
 		opts = {
